d17d24e19485a92c8ad7461303a0d32e03c956d0
Jesús André Flores García <al07098334@tecmilenio.mx>
2026-08-19 18:47:21 -0600

    tercer commit

--- app/src/main/java/com/example/booksy/MainActivity.kt
+++ app/src/main/java/com/example/booksy/MainActivity.kt
@@ -2,15 +2,11 @@ package com.example.booksy
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.booksy.R.layout.activity_main
 
-// Se agregó "class MainActivity"
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Establece la interfaz visual
-        setContentView(activity_main)
+        setContentView(R.layout.activity_main)
     }
 }
