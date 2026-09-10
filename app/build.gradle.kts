4802872d5c17836436f7aa8d96aad4aecc611a88
nataliatecmi <al02989529@tecmilenio.mx>
2026-09-09 21:27:28 -0600

    Dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,6 +83,10 @@ dependencies {
     implementation("androidx.fragment:fragment-ktx:1.6.2")
     implementation("androidx.viewpager2:viewpager2:1.1.0")
 
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
     // Compose
     val composeBom = platform(libs.androidx.compose.bom)
     implementation(composeBom)
